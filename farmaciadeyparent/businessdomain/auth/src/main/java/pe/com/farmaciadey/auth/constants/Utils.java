9246ddf4d51e2a22d1d5c2a43be431d864b762e3
Pablo Huerta <pabloh1545@gmail.com>
2025-11-12 23:22:50 -0500

    fix_jwt

--- farmaciadeyparent/businessdomain/auth/src/main/java/pe/com/farmaciadey/auth/constants/Utils.java
+++ farmaciadeyparent/businessdomain/auth/src/main/java/pe/com/farmaciadey/auth/constants/Utils.java
@@ -2,7 +2,17 @@ package pe.com.farmaciadey.auth.constants;
 
 public class Utils {
     private Utils() {}
-    public final static String secretKey = "357638792F423F4428472B4B6250655368566D597133743677397A2443264629";
+    
+    /**
+     * Clave secreta JWT de 512 bits (64 bytes) en hexadecimal
+     * Generada específicamente para seguridad en producción
+     * ¡NUNCA compartir esta clave!
+     */
+    public final static String secretKey = "4A614E645267556B58703273357538782F413F4428472B4B6250655368566D597133743677397A24432646294A614E645267556B58703273357538782F413F442847";
+    
+    /**
+     * Tiempo de expiración del JWT: 6 horas (en milisegundos)
+     */
     public final static Integer jwtExpirationTime = 60 * 60 * 1000 * 6;
 
     public final static Integer REQUEST_OK = 1;
