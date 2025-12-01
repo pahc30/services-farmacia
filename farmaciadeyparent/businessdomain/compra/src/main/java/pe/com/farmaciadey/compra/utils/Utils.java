88e1018a0fbac3566a46a12aa0a6e5722b315d47
Pablo Huerta <pabloh1545@gmail.com>
2025-12-01 11:32:26 -0500

    fix

--- farmaciadeyparent/businessdomain/compra/src/main/java/pe/com/farmaciadey/compra/utils/Utils.java
+++ farmaciadeyparent/businessdomain/compra/src/main/java/pe/com/farmaciadey/compra/utils/Utils.java
@@ -1,8 +1,23 @@
 package pe.com.farmaciadey.compra.utils;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Utils {
     public final static Integer REQUEST_OK = 1;
     public final static Integer REQUEST_ERROR = 0;
-    public final static String BASE_URL_PRODUCTO = "http://farmacia-producto:7013/producto";
-    public final static String BASE_URL_METODO_PAGO = "http://farmacia-metodopago:7014/metodopago";
+    
+    public static String BASE_URL_PRODUCTO;
+    public static String BASE_URL_METODO_PAGO;
+    
+    @Value("${service.producto.url:http://farmacia-producto:7013/producto}")
+    public void setBaseUrlProducto(String url) {
+        BASE_URL_PRODUCTO = url;
+    }
+    
+    @Value("${service.metodopago.url:http://farmacia-metodopago:7014/metodopago}")
+    public void setBaseUrlMetodoPago(String url) {
+        BASE_URL_METODO_PAGO = url;
+    }
 }
